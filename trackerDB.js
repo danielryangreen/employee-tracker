d4e8c077a1d31bb28b325698211d333cb89064b0
Dan Green <dan.ryan.green@gmail.com>
2021-03-06 16:23:14 -0700

    Read and console log each table

--- trackerDB.js
+++ trackerDB.js
@@ -8,8 +8,41 @@ const connection = mysql.createConnection({
   database: 'trackerDB',
 });
 
+const queryDepartments = () => {
+  connection.query('SELECT * FROM department', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, name }) => {
+      console.log(`${id} | ${name}`);
+    });
+    console.log('----------------------------------------');
+  });
+};
+
+const queryRoles = () => {
+  connection.query('SELECT * FROM role', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, title, salary }) => {
+      console.log(`${id} | ${title} | ${salary}`);
+    });
+    console.log('----------------------------------------');
+  });
+};
+
+const queryEmployees = () => {
+  connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err;
+    res.forEach(({ id, first_name, last_name }) => {
+      console.log(`${id} | ${first_name} | ${last_name}`);
+    });
+    console.log('----------------------------------------');
+  });
+};
+
 connection.connect((err) => {
   if (err) throw err;
   console.log(`Connected as id ${connection.threadId}`);
+  queryDepartments();
+  queryRoles();
+  queryEmployees();
   connection.end();
 });
